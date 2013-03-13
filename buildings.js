7834f265102f733201af47227859492b849abbbc
Trevor McCauley <senocular@hotmail.com>
2013-03-13 08:02:45 -0700

    added requestAnimationFrame polyfill

--- buildings.js
+++ buildings.js
@@ -387,3 +387,32 @@ Texture.prototype.draw = function(a,b,c,d,x,y){
 		this.dimensions.x, this.dimensions.y, this.dimensions.w, this.dimensions.h,
 		0, 0, this.dimensions.w, this.dimensions.h)
 }
+
+/**
+ * requestAnimationFrame polyfill b yErik Möller
+ * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ */
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelRequestAnimationFrame = window[vendors[x]+
+          'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}())
